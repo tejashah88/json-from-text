412e02dddd1eea63fb314c5b78af759fb1e6184c
Tejas Shah <tejashah88@gmail.com>
2017-10-17 12:07:10 -0700

    json_from_text now accepts an options object, with a new parameter for transforming json strings

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 var jsonic = require('jsonic');
 var { matchRecursive } = require('xregexp');
 
-function json_from_text(raw_text, parse_to_json) {
+function json_from_text(raw_text, options) {
+  if (options.parse_to_json === undefined) options.parse_to_json = true;
+  if (options.transform_function === undefined || !options.parse_to_json)
+    options.transform_function = function(json_string) { return json_string };
+
   var raw_results = matchRecursive(raw_text, '[{\\[]', '[\\]}]', 'g', { valueNames: ['text', 'left', 'center', 'right'] });
   var text_results = [], json_results = [], full_results = [];
 
   for (var i = 0; i < raw_results.length; i++) {
     if (raw_results[i].name == 'left') {
-      var json_string = raw_results[i].value + raw_results[i + 1].value + raw_results[i + 2].value;
-      var json_object = !!parse_to_json ? jsonic(json_string) : json_string;
+      var json_string = options.transform_function(raw_results[i].value + raw_results[i + 1].value + raw_results[i + 2].value);
+      console.log(json_string)
+      var json_object = !!options.parse_to_json ? jsonic(json_string) : json_string;
       json_results.push(json_object);
       full_results.push({ type: 'json', value: json_object });
       i += 2;
